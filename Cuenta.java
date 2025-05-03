a6d0fcc6d97a98c44d69664650aa4d33b82935e0
Arbey <vilmarbey@gmail.com>
2025-05-03 07:17:26 -0500

    cap3 V2.0

--- Cuenta.java
+++ Cuenta.java
@@ -1,10 +1,35 @@
-//fig. 3.1: Cuenta.java
-//Clase cuenta que contiene una variable de instancia llamada nombre 
-//y metodos para establecer y obtener su valor
+//fig. 3.8: Cuenta.java
+// La clase Cuenta con una variable de instancia double llamada saldo y un constructor
+// además de un método llamado deposito que realiza validación.
 
 public class Cuenta 
 {
 	private String nombre; //variable de instancia
+	private double saldo; //variable de instancia
+	
+	//el contructor Cuenta que recibe dos parametros
+	public Cuenta (String nombre, double saldo)
+	{
+		this.nombre = nombre;//asigna nombre a la variable de instancia nombre
+		
+		// valida que el saldo sea mayor que 0.0; de lo contrario,
+		// la variable de instancia saldo mantiene su valor inicial predeterminado de 0.0
+		if  (saldo > 0.0)//si el saldo es valido
+			this.saldo = saldo; //lo asigna a la variable de instancia saldo
+	}
+	
+	// método que deposita (suma) sólo una cantidad válida al saldo
+	public void depositar(double montoDeposito)
+	{
+		if(montoDeposito > 0.0) //Si el monto deposito es valido
+			saldo = saldo + montoDeposito;//lo suma al saldo
+	}
+	
+	// método que devuelve el saldo de la cuenta
+	public double obtenerSaldo()
+	{
+		return saldo;
+	}
 	
 	//metodo para establecer el nombre del objeto\
 	public void establecerNombre(String nombre) 
--- PruebaCuenta.java
+++ PruebaCuenta.java
@@ -1,28 +1,43 @@
-//fig. 3.2: PruebaCuenta.java
-//crear y manipular un objeto Cuenta
+// Fig. 3.9: PruebaCuenta.java
+// Entrada y salida de números de punto flotante con objetos Cuenta.
 import java.util.Scanner;
 
 public class PruebaCuenta 
 {
 	public static void main(String [] args)
 	{
-		//Crea un objeto Scanner para obtener la entrada desde el simbolo del sistema 
+		Cuenta cuenta1 = new Cuenta("Jane Green", 50.00);
+		Cuenta cuenta2 = new Cuenta("John Blue", 7.53);
+		
+		//muestra el saldo inicial de cada objeto 
+		System.out.printf("Saldo de %s: $%.2f%n", cuenta1.obtenerNombre(), cuenta1.obtenerSaldo());
+		System.out.printf("Saldo de %s: $%.2f%n%n", cuenta2.obtenerNombre(), cuenta2.obtenerSaldo());
+		
+		// crea un objeto Scanner para obtener la entrada de la ventana de comandos
 		Scanner entrada = new Scanner(System.in);
 		
-		//Crea un objeto cuenta y lo asigna a miCuenta
-		Cuenta miCuenta = new Cuenta();
+		System.out.println("Escriba el monto a depositar para cuenta1"); // indicador (promt)
+		double montoDeposito = entrada.nextDouble(); // obtiene entrada del usuario
+		System.out.printf("%nSumando %.2f al saldo de cuenta1%n%n",
+				montoDeposito);
+		cuenta1.depositar(montoDeposito); // suma al saldo de cuenta1
 		
-		//muestra el valor inicial del nombre (null)
-		System.out.printf("El nombre inicial es: %s%n%n", miCuenta.obtenerNombre());
+		//muestra los saldos
+		System.out.printf("Saldo de %s: $%.2f%n",
+				cuenta1.obtenerNombre(), cuenta1.obtenerSaldo());
+		System.out.printf("saldo de %s: $%.2f%n%n",
+				cuenta2.obtenerNombre(), cuenta2.obtenerSaldo());
 		
-		//pide y lee el nombre
-		System.out.println("Introduzca el nombre:");
-		String elNombre = entrada.nextLine();//lee una linea de texto
-		miCuenta.establecerNombre(elNombre);//coloca el nombre n mi cuenta
-		System.out.println();//Imprime una linea en blanco
+		System.out.print("Escriba el monto a depositar para la cuenta2");
+		montoDeposito = entrada.nextDouble(); //obtiene entrada del usuario
+		System.out.printf("%nsumando %.2f al saldo de cuenta%n%n",
+				montoDeposito);
+		cuenta2.depositar(montoDeposito);//Suma al saldo de cuenta2
 		
-		//Muestra el nombre almacenado en el objeto miCuenta
-		System.out.printf("El nombre en el objeto miCueta es: %n%S%n", 
-				miCuenta.obtenerNombre());
+		//muestra los saldos
+		System.out.printf("Saldo de %s: $%.2f%n", cuenta1.obtenerNombre(), cuenta1.obtenerSaldo());
+
+		System.out.printf("Saldo de %s: $%.2f%n%n", cuenta2.obtenerNombre(), cuenta2.obtenerSaldo());
+
 	}
 }//Fin de la clase PruebaCuenta
